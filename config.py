cc81e33f9897db5c31a8bdf5b50557cd9a4f9da0
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-05-04 17:38:23 +0300

    Add secret key instance

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-# Configuration variables
+import os
 
 class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'cut-the-bs'
 	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:myPassword@localhost/blogger'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
